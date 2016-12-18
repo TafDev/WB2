5a3b407f6839fa4a7b41cc308daefcfe5fd51013
TafDev <chombot@hotmail.com>
2016-12-18 04:14:32 +0000

    adding gitignore files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
   config.action_cable.allowed_request_origins = ['https://mysterious-ridge-90718.herokuapp.com']
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  config.assets.compile = true
 
   config.action_cable.url = "wss://mysterious-ridge-90718.herokuapp.com/cable"
 
